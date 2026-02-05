4c140ddec46705bb3958c28ae02b024a8520bf2a
Viacheslav Ravdin <viacheslav@hoo.lt>
2026-02-05 07:44:09 +0200

    Some lines added to products repository

---
+++ src/Infrastructure/Products/Repository.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Hoo\ProductFeeds\Infrastructure\Products;
+
+use wpdb;
+
+class Repository {
+	protected wpdb $wpdb;
+
+	public function __construct() {
+		global $wpdb;
+
+		$this->wpdb = $wpdb;
+	}
+
+	public function get() {
+		return $this->wpdb->get_results("
+        SELECT ID, post_title, post_content, post_excerpt
+        FROM {$this->wpdb->posts}
+        WHERE post_type = 'product'
+        AND post_status = 'publish'
+    ", ARRAY_A);
+	}
+}
