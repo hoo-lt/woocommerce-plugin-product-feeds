ed2c4b5f124fcdea5e6984d9fb1e48fcc812a047
Viacheslav Ravdin <viacheslav@hoo.lt>
2026-02-07 16:36:47 +0200

    Updated SQL

--- src/Infrastructure/Clients/Taxonomy/Client.php
+++ src/Infrastructure/Clients/Taxonomy/Client.php
@@ -21,22 +21,21 @@ class Client
 
 	public function getExcludedIds(): array
 	{
-		return array_unique($this->wpdb->get_col($this->wpdb->prepare(
-			"WITH RECURSIVE term_excluded AS (
-				SELECT term_taxonomy.term_taxonomy_id, term_taxonomy.term_id, term_taxonomy.parent
-				FROM {$this->wpdb->termmeta} termmeta
-				INNER JOIN {$this->wpdb->term_taxonomy} term_taxonomy ON termmeta.term_id = term_taxonomy.term_id
-				WHERE termmeta.meta_key = %s AND termmeta.meta_value = %s
+		return $this->wpdb->get_col($this->wpdb->prepare(
+			"WITH RECURSIVE excluded_tree AS (
+        SELECT term_taxonomy.term_taxonomy_id, term_taxonomy.term_id
+        FROM {$this->wpdb->term_taxonomy} term_taxonomy
+        INNER JOIN {$this->wpdb->termmeta} termmeta ON term_taxonomy.term_id = termmeta.term_id
+        WHERE termmeta.meta_key = %s AND termmeta.meta_value = %s
 
-				UNION ALL
+        UNION
 
-				SELECT term_taxonomy.term_taxonomy_id, term_taxonomy.term_id, term_taxonomy.parent
-				FROM {$this->wpdb->term_taxonomy} term_taxonomy
-				INNER JOIN term_excluded ON term_taxonomy.parent = term_excluded.term_id
-			)
-			SELECT term_taxonomy_id FROM term_excluded",
-			self::KEY,
-			Domain\Term::Exclude->value
-		)));
+        SELECT term_taxonomy.term_taxonomy_id, term_taxonomy.term_id
+        FROM {$this->wpdb->term_taxonomy} term_taxonomy
+        INNER JOIN excluded_tree ON term_taxonomy.parent = excluded_tree.term_id
+      ) SELECT term_taxonomy_id FROM excluded_tree",
+			'product_feeds',
+			'exclude'
+		));
 	}
 }
